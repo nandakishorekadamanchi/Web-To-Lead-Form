2bc78907f3d242f86a999f9d05812b6d93856d2f
Nanda Kishore Kadamanchi <nandakishorekadamanchi@gmail.com>
2024-03-01 12:20:26 +0530

    reCaptcha changes

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-function submithandler(){
-    let outputdate = document.querySelector('.outputdate')
-    let inputdate = document.querySelector('.inputdate')
-    console.log('inputdate.value', inputdate.value) //string -→> date (en_IN)
-    //typeof inputdate.value is 'String'
-    //system.debug(userInfo.getlocale()); en_IN
+let captchachecked = false;
+function submithandler(event){
 
-    let formatteddate = new Date(inputdate.value).toLocaleDateString("en-IN");
-    outputdate.value = formatteddate;
+    if(captchachecked){
+        let outputdate = document.querySelector('.outputdate')
+        let inputdate = document.querySelector('.inputdate')
+        console.log('inputdate.value', inputdate.value) //string --> date (en_IN)
+        //typeof inputdate.value is 'String'
+        //system.debug(userInfo.getlocale()); en_IN
+    
+        let formatteddate = new Date(inputdate.value).toLocaleDateString("en-IN");
+        outputdate.value = formatteddate;
+    }
+    else{
+        alert('Please Check the reCAPTCHA box to submit the lead');
+        event.preventDefault();
+    }
+   
 }
 
 
@@ -19,3 +28,7 @@ function timestamp() {
     } 
 } 
 setInterval(timestamp, 500); 
+
+function captchasuccess(){
+    captchachecked = true;
+}
